5957617a24032e390389cdb09233234401b59c83
Mick Morrow <mmorrow24work@gmail.com>
2026-03-11 16:58:35 +0000

    Update postStart.sh

--- .devcontainer/postStart.sh
+++ .devcontainer/postStart.sh
@@ -5,16 +5,7 @@ echo "logs:/workspaces/.codespaces/.persistedshare" > nautobot-only.txt
 echo "User=admin / Password=admin" >> nautobot-only.txt
 echo "docker exec -it nautobot-lab nautobot-server createsuperuser" >> nautobot-only.txt
 
-# docker run -itd --name nautobot-lab -p 8082:8000 --hostname nautobot-lab.local networktocode/nautobot-lab
-
-docker run -itd \
-  --name nautobot-lab \
-  -p 8082:8000 \
-  --hostname nautobot-lab.local \
-  -e SUPERUSER_NAME=admin \
-  -e SUPERUSER_PASSWORD=admin \
-  -e SUPERUSER_EMAIL=admin@example.com \
-  networktocode/nautobot-lab
+docker run -itd --name nautobot-lab -p 8082:8000 --hostname nautobot-lab.local networktocode/nautobot-lab
 
 # URLs (Codespaces auto-forwards)
 echo "Nautobot UI: http://localhost:8082"
