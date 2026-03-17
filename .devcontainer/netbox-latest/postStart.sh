eea8356e5ebfc9689e6685ca843726623701eac9
Mick Morrow <mmorrow24work@gmail.com>
2026-03-17 09:48:42 +0000

    Update postStart.sh

--- .devcontainer/netbox-latest/postStart.sh
+++ .devcontainer/netbox-latest/postStart.sh
@@ -1,1 +1,13 @@
-a
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "logs:/workspaces/.codespaces/.persistedshare" > nautobot-only.txt
+echo "eval '$(poetry env activate)'" >> nautobot-only.txt
+echo "invoke stop && invoke start" >> nautobot-only.txt
+echo "User=admin / Password=admin" >> nautobot-only.txt
+echo "docker exec -it nautobot_docker_compose-nautobot-1 nautobot-server createsuperuser" >> nautobot-only.txt
+
+
+# URLs (Codespaces auto-forwards)
+echo "Netbox UI: http://localhost:8080"
+echo "Netbox Login User=admin / Password=admin"
