4c88f2adbf720bf2c6529cbadd5742ea91fbd0cc
Mick Morrow <mmorrow24work@gmail.com>
2026-03-17 00:16:22 +0000

    Update postStart.sh

--- .devcontainer/nautobot-latest/postStart.sh
+++ .devcontainer/nautobot-latest/postStart.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo "logs:/workspaces/.codespaces/.persistedshare" > nautobot-only.txt
-echo "eval "$(poetry env activate)"" >> nautobot-only.txt
+echo "eval '$(poetry env activate)'" >> nautobot-only.txt
 echo "invoke stop && invoke start" >> nautobot-only.txt
 echo "User=admin / Password=admin" >> nautobot-only.txt
 echo "docker exec -it nautobot_docker_compose-nautobot-1 nautobot-server createsuperuser" >> nautobot-only.txt
